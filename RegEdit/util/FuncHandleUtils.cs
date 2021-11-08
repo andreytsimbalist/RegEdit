ddc37fc2677f57757fc343a65596c5798454923f
Artsem Silantsyeu <artsem.silantsyeu@frox.ch>
2021-11-08 22:18:57 +0300

    add git ignore

---
+++ RegEdit/util/FuncHandleUtils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace RegEdit.util
+{
+    public static class FuncHandleUtils
+    {
+        public static TResult Execute<TResult>(Func<TResult> func, TResult defaultResult)
+        {
+            try
+            {
+                return func.Invoke();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return defaultResult;
+            }
+        }
+    }
+}
